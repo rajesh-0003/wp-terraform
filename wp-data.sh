41d91c4c6a7c7eddcb19c9ec6fec8886e133584e
rajesh-0003 <rajesh.p3rkr@gmail.com>
2024-10-24 16:30:08 +0530

    wp-data.sh

--- wp-data.sh
+++ wp-data.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-sudo git clone https://github.com/rajesh-0003/dockerfilerp.git
-sudo yum install docker -y
+sudo yum -y install git
+sudo yum -y install docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 sudo chmod 666 /var/run/docker.sock
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/veerareddy1422/wordpress.git
+cd wordpress/
 docker-compose up -d
